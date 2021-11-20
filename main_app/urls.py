5532b321a288efbe41ca91a7c692f1b2ca542d0a
Lloyd Eddy <lloyd@lloyds-mbp.home>
2021-11-20 13:19:31 -0500

    WIP provides full-CRUD

--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('syms/', views.syms_index, name='index'),
     path('syms/<int:sym_id>/', views.syms_detail, name='detail'),
+    path('syms/create/', views.SymCreate.as_view(), name='syms_create'),
+    path('syms/<int:pk>/update/', views.SymUpdate.as_view(), name='syms_update'),
+    path('syms/<int:pk>/delete/', views.SymDelete.as_view(), name='syms_delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Sym
 
 # Create your views here.
@@ -16,3 +17,15 @@ def syms_index(request):
 def syms_detail(request, sym_id):
     sym = Sym.objects.get(id=sym_id)
     return render(request, 'syms/detail.html', { 'sym': sym })
+
+class SymCreate(CreateView):
+    model = Sym
+    fields = '__all__'
+
+class SymUpdate(UpdateView):
+    model = Sym
+    fields = ['gender', 'occupation', 'description']
+
+class SymDelete(DeleteView):
+    model = Sym
+    success_url = '/syms/'
